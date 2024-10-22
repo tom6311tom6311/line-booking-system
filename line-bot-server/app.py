5d33bc987486a6fccf9d43c0236edfa12328bf55
Tom Huang <tom6311tom6311@gmail.com>
2024-10-22 22:33:19 +0800

    refactor: adjust the lookup by check-in-date flow to align user-prompted pattern
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- line-bot-server/app.py
+++ line-bot-server/app.py
@@ -1,12 +1,13 @@
+import re
 import json
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, PostbackEvent, TextMessage, TextSendMessage,  QuickReply, QuickReplyButton, DatetimePickerAction
 from const import db_config
-from const.line_config import LINE_CHANNEL_ACCESS_TOKEN, LINE_CHANNEL_SECRET, USER_COMMAND_SEARCH_BOOKING_BY_KEYWORD, POSTBACK_COMMAND_SEARCH_BOOKING_BY_CHECK_IN_DATE, POSTBACK_COMMAND_VIEW_FULL_BOOKING_INFO
+from const.line_config import LINE_CHANNEL_ACCESS_TOKEN, LINE_CHANNEL_SECRET, USER_COMMAND_SEARCH_BOOKING_BY_KEYWORD, USER_COMMAND_SEARCH_BOOKING_BY_CHECK_IN_DATE, POSTBACK_COMMAND_SEARCH_BOOKING_BY_CHECK_IN_DATE, POSTBACK_COMMAND_VIEW_FULL_BOOKING_INFO
 from data_access.booking_dao import BookingDAO
-from utils.datetime_utils import is_valid_date
+from utils.datetime_utils import is_valid_date, extract_date_from_string_template
 from utils.booking_utils import format_booking_info
 from utils.line_messaging_utils import create_booking_carousel_message
 
@@ -58,6 +59,7 @@ def handle_message(event):
   user_message = event.message.text
   app.logger.debug(f"User message: {user_message}")
 
+  reply_messages = []
   if user_message == USER_COMMAND_SEARCH_BOOKING_BY_KEYWORD:
     quick_reply_buttons = [
       QuickReplyButton(action=DatetimePickerAction(
@@ -68,25 +70,29 @@ def handle_message(event):
     ]
 
     quick_reply = QuickReply(items=quick_reply_buttons)
-    line_bot_api.reply_message(
-      event.reply_token,
-      TextSendMessage(text="請提供關鍵字:\n(ID、電話末3碼、姓名)", quick_reply=quick_reply)
-    )
+    reply_messages.append(TextSendMessage(text="請提供關鍵字:\n(ID、電話末3碼、姓名)", quick_reply=quick_reply))
+
+  elif date := extract_date_from_string_template(USER_COMMAND_SEARCH_BOOKING_BY_CHECK_IN_DATE, user_message):
+    matches = booking_dao.search_booking_by_check_in_date(date)
+    if not matches:
+      reply_messages.append(TextSendMessage(text="找不到任何訂單"))
+    else:
+      reply_messages.append(create_booking_carousel_message(matches))
+
   else:
     # Assuming the user provides a keyword
     keyword = user_message
     matches = booking_dao.search_booking_by_keyword(keyword)
 
     if not matches:
-      reply_message = TextSendMessage(text="找不到任何訂單")
+      reply_messages.append(TextSendMessage(text="找不到任何訂單"))
     else:
-      reply_message = create_booking_carousel_message(matches)
+      reply_messages.append(create_booking_carousel_message(matches))
 
-    # Send the reply (either results or no matches)
-    line_bot_api.reply_message(
-      event.reply_token,
-      reply_message
-    )
+  line_bot_api.reply_message(
+    event.reply_token,
+    reply_messages
+  )
 
 @handler.add(PostbackEvent)
 def handle_message_postback(event):
@@ -97,36 +103,25 @@ def handle_message_postback(event):
     app.logger.error("Failed to parse postback event data as json")
     return
 
+  reply_messages = []
   if command_obj['command'] == POSTBACK_COMMAND_SEARCH_BOOKING_BY_CHECK_IN_DATE:
     selected_date = event.postback.params['date']
 
-    reply_messages = []
-    reply_messages.append(TextSendMessage(text=f"查詢{selected_date.replace('-', '/')}入住的訂單"))
+    reply_messages.append(TextSendMessage(USER_COMMAND_SEARCH_BOOKING_BY_CHECK_IN_DATE.format(date=selected_date.replace('-', '/'))))
 
-    matches = booking_dao.search_booking_by_check_in_date(selected_date)
-    if not matches:
-      reply_messages.append(TextSendMessage(text="找不到任何訂單"))
-    else:
-      reply_messages.append(create_booking_carousel_message(matches))
-
-    line_bot_api.reply_message(
-      event.reply_token,
-      reply_messages
-    )
   elif command_obj['command'] == POSTBACK_COMMAND_VIEW_FULL_BOOKING_INFO:
     booking_id = command_obj['booking_id']
     booking_info = booking_dao.get_booking_info(int(booking_id))
 
-    reply_messages = []
-    reply_messages.append(TextSendMessage(text=f"檢視訂單 #{booking_id}"))
     reply_messages.append(TextSendMessage(format_booking_info(booking_info, 'normal') or "找不到ID對應的訂單"))
 
-    line_bot_api.reply_message(
-      event.reply_token,
-      reply_messages
-    )
   else:
     app.logger.warning(f"Unrecognized postback command: {command_obj['command']}")
 
+  if (len(reply_messages) > 0):
+    line_bot_api.reply_message(
+        event.reply_token,
+        reply_messages
+      )
 if __name__ == "__main__":
   app.run(debug=True)
--- line-bot-server/utils/datetime_utils.py
+++ line-bot-server/utils/datetime_utils.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 
 def is_valid_date(date_str):
@@ -6,3 +7,18 @@ def is_valid_date(date_str):
     return True
   except ValueError:
     return False
+
+def extract_date_from_string_template(template, input_str):
+  # Define a pattern to match the date in yyyy/mm/dd format
+  pattern = re.sub(r'{date}', r'(\d{4})(/|-)(\d{2})(/|-)(\d{2})', template)
+
+  # Search for the date pattern in the input string
+  match = re.search(pattern, input_str)
+
+  if match:
+    # Extract the matched groups and reformat into yyyy-mm-dd
+    year, month, day = match.groups()
+    parsed_date = f"{year}-{month}-{day}"
+    return parsed_date
+  else:
+    return None
